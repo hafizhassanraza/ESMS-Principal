7dffa1065758a95e9f9464b678c284cf585766f0
Shah Alam <shahalam2590@gmail.com>
2022-10-24 15:45:21 +0500

    Attendance Pie Chart Implemented

--- app/src/main/java/com/enfotrix/principalportal/activities/MainActivity.java
+++ app/src/main/java/com/enfotrix/principalportal/activities/MainActivity.java
@@ -3,12 +3,15 @@ package com.enfotrix.principalportal.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.enfotrix.principalportal.R;
 import com.enfotrix.principalportal.databinding.ActivityMainBinding;
 import com.enfotrix.principalportal.utilities.Constants;
 import com.enfotrix.principalportal.utilities.PreferenceManager;
+
+import org.eazegraph.lib.models.PieModel;
 
 import java.util.Locale;
 
@@ -30,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void setPreferredInfo() {
         binding.actionBarLayout.tvActionBar.setText(preferenceManager.getString(Constants.KEY_NAME).toUpperCase(Locale.ROOT));
+        setAttendancePieChart();
+    }
+
+    private void setAttendancePieChart() {
+        binding.pieChart.addPieSlice(
+                new PieModel(
+                        "Present",
+                        60,
+                        Color.parseColor("#76DD7B")
+                ));
+        binding.pieChart.addPieSlice(
+                new PieModel(
+                        "Absent",
+                        20,
+                        Color.parseColor("#EB3F62")));
+        binding.pieChart.addPieSlice(
+                new PieModel(
+                        "Leave",
+                        20,
+                        Color.parseColor("#61B8E0")
+                ));
+        binding.pieChart.startAnimation();
     }
 
     private void setListeners() {
